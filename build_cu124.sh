2e3c2fa032b0cd56fcdb1ce99dde9511fee077eb
leslie2046 <253605712@qq.com>
2026-07-09 21:49:59 +0800

    modify

--- build_cu124.sh
+++ build_cu124.sh
@@ -1,1 +1,9 @@
-docker buildx build   --memory=8g --build-arg http_proxy=http://192.168.0.188:21026   --build-arg https_proxy=http://192.168.0.188:21026   -t leslie2046/xinference:$1-cu124   -f xinference/deploy/docker/Dockerfile.cu124 .
+#!/bin/bash
+#docker buildx build   --memory=8g --build-arg http_proxy=http://192.168.0.188:21026   --build-arg https_proxy=http://192.168.0.188:21026   -t leslie2046/xinference:$1-cu124   -f xinference/deploy/docker/Dockerfile.cu124 .
+
+DOCKER_BUILDKIT=1 docker build \
+	--progress=plain \
+	--memory=8g \
+	--build-arg http_proxy=http://192.168.0.188:21026 \
+	--build-arg https_proxy=http://192.168.0.188:21026 -t leslie2046/xinference:$1-cu124 -f xinference/deploy/docker/Dockerfile.cu124 \
+		      .
